14e7e6dd00527b4b79f359356ec48e2b39cdbb79
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-08-23 15:26:18 +0100

    Reducers Set-up

---
+++ src/components/TotalDisplay.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const TotalDisplay = () => {
+
+    
+    return(
+    <div className="row">
+
+    </div>);
+}
+
+export default TotalDisplay;
---
+++ src/reducers/index.js
@@ -0,0 +1,58 @@
+export const initialState = {
+    total: 0,
+    operation: "+",
+    memory: 0
+}
+
+const calculateResult = (num1, num2, operation) => {
+    switch(operation) {
+        case("+"):
+            return num1 + num2;
+        case("*"):
+            return num1 * num2;
+        case("-"):
+            return num1 - num2;
+    }
+}
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case(ADD_ONE):
+            return({
+                ...state,
+                total: state.total + 1
+            });
+
+        case(APPLY_NUMBER):
+            return ({ 
+                ...state, 
+                total: calculateResult(state.total, action.payload, state.operation)
+            });
+        
+        case(CHANGE_OPERATION):
+            return ({
+                ...state,
+                operation: action.payload
+            });
+            
+        case (CLEAR_DISPLAY):
+            return ({ ...state, total: 0 });
+
+        case (MEMORY_ACTION_MPLUS):
+            return ({ ...state, memory: state.total });
+
+         case(MEMORY_ACTION_APPLY):
+            return ({ 
+                ...state, 
+                total: calculateResult(state.total, state.memory, state.operation)
+            });
+
+        case(MEMORY_ACTION_MC):
+            return ({ ...state, memory: 0 });
+
+        default:
+            return state;
+    }
+}
+
+export default reducer;
